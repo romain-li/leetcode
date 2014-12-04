290a1f66d44935d22588c5ecd578093764cced02
romain-li <romain_li@163.com>
2014-12-04 21:17:27 +0800

    Doing...
    Something wrong

--- problems/Median_of_Two_Sorted_Arrays/solution.py
+++ problems/Median_of_Two_Sorted_Arrays/solution.py
@@ -1,4 +1,32 @@
 class Solution:
     # @return a float
     def findMedianSortedArrays(self, A, B):
-        
+        length_a = len(A)
+        length_b = len(B)
+        median_index = (length_a + length_b) / 2
+
+        a1 = 0
+        a2 = length_a - 1
+        b1 = 0
+        b2 = length_b - 1
+
+        while (a1 != a2 and b1 != b2):
+            a_half = (a1 + a2) / 2
+            b_half = (b1 + b2) / 2
+
+            if (A[a_half] < B[b_half]):
+                if (a_half + b_half) < median_index:
+                    a1 = a_half if a1 != a_half else a2
+                else:
+                    b2 = b_half
+            else:
+                if (a_half + b_half) < median_index:
+                    b1 = b_half if b1 != b_half else b2
+                else:
+                    a2 = a_half
+
+        print a1, a2, b1, b2
+        if (a1 + 1 >= a2):
+            return B[b1 + median_index - a1 - 1]
+        else:
+            return A[a1 + median_index - b1 - 1]
--- problems/Median_of_Two_Sorted_Arrays/test.py
+++ problems/Median_of_Two_Sorted_Arrays/test.py
@@ -8,8 +8,25 @@ class TestCase(unittest.TestCase):
     def setUp(self):
         self.solution = Solution()
 
-    def testSolution(self):
-        pass
+    def testSolution01(self):
+        A = (1, 3, 5, 7, 9, 11)
+        B = (2, 4, 6, 8, 10)
+        self.assertEqual(self.solution.findMedianSortedArrays(A, B), 6)
+
+    def testSolution02(self):
+        A = (1, 3, 4, 5, 7, 9, 11)
+        B = (2, 6, 8, 10)
+        self.assertEqual(self.solution.findMedianSortedArrays(A, B), 6)
+
+    def testSolution03(self):
+        A = (1, 2, 3, 4, 5, 9)
+        B = (6, 7, 8, 10, 11)
+        self.assertEqual(self.solution.findMedianSortedArrays(A, B), 6)
+
+    def testSolution04(self):
+        A = (1, 2, 3, 4, 5, 6)
+        B = (7, 8, 9, 10, 11)
+        self.assertEqual(self.solution.findMedianSortedArrays(A, B), 6)
 
 
 def test():
