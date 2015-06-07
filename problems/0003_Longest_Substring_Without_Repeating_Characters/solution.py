d5bd3a31cb0f238ad5b236b4095776b545ffd446
lixue <romain_li@163.com>
2015-06-07 19:55:45 +0800

    Problem 3 solved

--- problems/0003_Longest_Substring_Without_Repeating_Characters/solution.py
+++ problems/0003_Longest_Substring_Without_Repeating_Characters/solution.py
@@ -2,4 +2,26 @@ class Solution:
     # @param {string} s
     # @return {integer}
     def lengthOfLongestSubstring(self, s):
-        
+        prev_map = self.prev_map(s)
+        max_length = 0
+        start = 0
+        for i, c in enumerate(s):
+            prev = prev_map[i]
+            if prev >= start:
+                start = prev + 1
+            max_length = max(max_length, i - start + 1)
+        return max_length
+
+    @staticmethod
+    def prev_map(s):
+        """
+        Generate the previous character position map.
+        For example: "abcabcbb" -> [None, None, None, 0, 1, 2, 4, 6]
+        """
+        prev_map = []
+        char_map = {}
+        for i, c in enumerate(s):
+            prev = char_map.get(c)
+            char_map[c] = i
+            prev_map.append(prev)
+        return prev_map
--- problems/0003_Longest_Substring_Without_Repeating_Characters/test.py
+++ problems/0003_Longest_Substring_Without_Repeating_Characters/test.py
@@ -4,12 +4,17 @@ from solution import Solution
 
 
 class TestCase(unittest.TestCase):
-
     def setUp(self):
         self.solution = Solution()
 
-    def test_solution(self):
-        pass
+    def test_solution1(self):
+        self.assertEqual(self.solution.lengthOfLongestSubstring('abcabcbb'), 3)
+
+    def test_solution2(self):
+        self.assertEqual(self.solution.lengthOfLongestSubstring('bbbbb'), 1)
+
+    def test_solution3(self):
+        self.assertEqual(self.solution.lengthOfLongestSubstring('abcabcdefabcdefg'), 7)
 
 
 def test():
